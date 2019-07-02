c6c46382b180a5b86de493912f14e5e453ce4728
AC012018 <stephen.wanyingo@gmail.com>
2019-07-02 14:23:26 +0300

    Initial commit

---
+++ app/src/main/java/com/example/calculators2/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.calculators2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button one;
+    private Button two;
+    private Button three;
+    private Button four;
+    private Button five;
+    private Button six;
+    private Button seven;
+    private Button eight;
+    private Button nine;
+    private Button division;
+    private Button multiplication;
+    private Button addition;
+    private Button subtaction;
+    private Button decimal;
+    private Button zero;
+    private Button percentage;
+    private Button delete;
+    private Button equal;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
